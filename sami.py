814502c64fb6abde5fbaa92281f01963cca2481c
kraus-s <sven.kraus@unibas.ch>
2021-11-11 14:22:01 +0100

    Initial commit

---
+++ sami.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.title("Das Recht auf eigene Sprache: Die Situation der Sami in Norwegen")
+st.write("Diese WebApp stellt die Ergebnisse der Schweizer Jugend Forscht Studienwoche zum Thema Menschenrechte am Seminar für Nordistik der Universität Basel vor.")
+st.sidebar.write("Inhaltsverzeichnis")
